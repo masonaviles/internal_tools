7f9ea49494bec363a9667c7bfef83d2176742d63
Mason Aviles <marian_aviles@apple.com>
2021-11-08 22:39:09 -0800

    prefix the headers

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let text = document.body.innerText;
+let headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6'), i;
+
+for (i = 0; i < headers.length; ++i) {
+    headers[i].innerText = "**========[HEADER]========** " + headers[i].innerText;
+}
 
 function download_txt() {
     var textToSave = document.body.innerText;
@@ -9,8 +14,8 @@ function download_txt() {
     hiddenElement.download = 'copy.txt';
     hiddenElement.click();
 }
-  
-document.getElementById('Button').addEventListener('click', download_txt);
+
+download_txt();
 
 javascript:(function(){
     var textToSave = document.body.innerText;
@@ -22,5 +27,27 @@ javascript:(function(){
     hiddenElement.target = '_blank';
     hiddenElement.download = `copy_content_${docTitle}.txt`;
     hiddenElement.click();
+
+})();
+
+javascript:(function(){
+    let text = document.body.innerText;
+    let headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6'), i;
+
+    for (i = 0; i < headers.length; ++i) {
+        headers[i].innerText = "[HEADER] " + headers[i].innerText;
+    }
+
+    function download_txt() {
+        var textToSave = document.body.innerText;
+        var hiddenElement = document.createElement('a');
+    
+        hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'copy.txt';
+        hiddenElement.click();
+    }
+
+    //download_txt();
 
 })();
